4247787b10b2906534a9df69dfe563071a53bfee
BMayUSA <BrianMay.USA@gmail.com>
2017-10-21 12:42:06 -0400

    AStar implementation

---
+++ atomic-rts/sdks/java/rts-client/src/com/atomicobject/rts/AStarNode.java
@@ -0,0 +1,31 @@
+public class AStarNode {
+	public AStarNode parent;
+	public int x;
+	public int y;
+	
+	public int gValue;
+	public int hValue;
+	public int isWall = false;
+	
+	public AStarNode(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public void setGValue(int amt) {
+		this.gValue = amt;
+	}
+	
+	public void calculateHValue(AStarNode dest) {
+		Math.abs(this.x - dest.x) + Math.abs(this.y - dest.y);
+	}
+	
+	public void calculateGValue(AStarNode point) {
+		this.gValue = point.gValue + 1;
+	}
+	
+	public int getFValue() {
+		return this.gValue + this.hValue;
+	}
+
+}
